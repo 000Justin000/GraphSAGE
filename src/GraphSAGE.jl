6b824642cad2cb3e5b2e99b6b148f3108c5b91ed
Junteng Jia <juntengjia@hotmail.com>
2019-10-12 16:01:30 -0400

    added MaxSAGE, SumSAGE

--- src/GraphSAGE.jl
+++ src/GraphSAGE.jl
@@ -6,16 +6,15 @@ module GraphSAGE
 
     export graph_encoder;
 
-    abstract type SAGE end;
-
-    # mean aggregator
-    struct MeanSAGE{F} <: SAGE
-        f::F;
+    # sampler & aggregator
+    struct SAGE{T}
+        F::T;
         k::Int;
+        A::Function;
     end
 
-    function (c::MeanSAGE)(G::AbstractGraph, node_list::Vector{Int})
-        f, k = c.f, c.k;
+    function (c::SAGE)(G::AbstractGraph, node_list::Vector{Int})
+        F, k, A = c.F, c.k, c.A;
 
         # perform weighted sample
         # note we are sampling without replacement here, hense the expected mean equals the true weighted average
@@ -29,7 +28,7 @@ module GraphSAGE
         # compute hidden vector of unique neighbors
         unique_nodes = union(node_list, sampled_nbrs_list...);
         u2i = Dict{Int,Int}(u=>i for (i,u) in enumerate(unique_nodes));
-        hh0 = f(G, unique_nodes);
+        hh0 = F(G, unique_nodes);
 
         @assert length(hh0) > 0 "non of the vertices has incoming edge"
         sz = size(hh0[1]);
@@ -37,33 +36,33 @@ module GraphSAGE
         # compute the mean hidden vector of the sampled neighbors
         hh1_ = Vector{AbstractVector}();
         for (u, sampled_nbrs) in zip(node_list, sampled_nbrs_list)
-            h_nbrs = length(sampled_nbrs) != 0 ? sum(hh0[u2i[v]] for v in sampled_nbrs) / length(sampled_nbrs) : zeros(sz);
+            h_nbrs = length(sampled_nbrs) != 0 ? A([hh0[u2i[v]] for v in sampled_nbrs]) : zeros(sz);
             push!(hh1_, vcat(hh0[u2i[u]], h_nbrs));
         end
 
         return hh1_;
     end
 
-    Flux.@treelike MeanSAGE;
+    Flux.@treelike SAGE;
 
 
 
     # transformer
-    struct Transformer{F <:SAGE,T}
-        f::F;
+    struct Transformer{T}
+        S::SAGE;
         L::T;
     end
 
-    function Transformer(f::SAGE, dim_h0::Integer, dim_h1::Integer, σ=relu)
+    function Transformer(S::SAGE, dim_h0::Integer, dim_h1::Integer, σ=relu)
         L = Dense(dim_h0*2, dim_h1, σ);
 
-        return Transformer(f, L);
+        return Transformer(S, L);
     end
 
     function (c::Transformer)(G::AbstractGraph, node_list::Vector{Int})
-        f, L = c.f, c.L;
+        S, L = c.S, c.L;
 
-        hh1 = L.(f(G, node_list));
+        hh1 = L.(S(G, node_list));
 
         return hh1;
     end
@@ -74,27 +73,30 @@ module GraphSAGE
 
     # graph encoder
     function graph_encoder(features::Vector, dim_in::Integer, dim_out::Integer, dim_h::Integer, layers::Vector{String}, ks::Vector{Int}, σ=relu)
-        @assert length(layers) > 0 "number of layers must be positive"
+        @assert length(layers) > 0
+        @assert length(layers) == length(ks)
+
+        S2A = Dict{String,Function}("MeanSAGE" => (x->mean(x)), "MaxSAGE" => (x->max.(x...)), "SumSAGE" => (x->sum(x)));
 
         # first aggregator always pull input features
-        agg = (@eval $(Symbol(layers[1])))((G,node_list) -> features[node_list], ks[1]);
+        sage = SAGE((G,node_list) -> features[node_list], ks[1], S2A[layers[1]]);
         if length(layers) == 1
             # single layer, directly output
-            tsf = Transformer(agg, dim_in, dim_out, σ);
+            tsfm = Transformer(sage, dim_in, dim_out, σ);
         else
             # multiple layer, first encode to hidden
-            tsf = Transformer(agg, dim_in, dim_h, σ);
+            tsfm = Transformer(sage, dim_in, dim_h, σ);
 
             # the inner layers, hidden to hidden
             for i in 2:length(layers)-1
-                agg = (@eval $(Symbol(layers[i])))(tsf, ks[i]);
-                tsf = Transformer(agg, dim_h, dim_h, σ);
+                sage = SAGE(tsfm, ks[i], S2A[layers[i]]);
+                tsfm = Transformer(sage, dim_h, dim_h, σ);
             end
 
-            agg = (@eval $(Symbol(layers[end])))(tsf, ks[end]);
-            tsf = Transformer(agg, dim_h, dim_out, σ);
+            sage = SAGE(tsfm, ks[end], S2A[layers[end]]);
+            tsfm = Transformer(sage, dim_h, dim_out, σ);
         end
 
-        return tsf;
+        return tsfm;
     end
 end
