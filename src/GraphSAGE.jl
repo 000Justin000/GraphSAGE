787728448a5e5fa15bce6454e375228a1db78199
Junteng Jia <juntengjia@hotmail.com>
2019-10-25 09:47:05 -0400

    clean up edge features

--- src/GraphSAGE.jl
+++ src/GraphSAGE.jl
@@ -11,36 +11,31 @@ module GraphSAGE
         L::F;
     end
 
-    function AGG(S::String, dim_h::Int, dim_e::Int, σ=relu)
+    function AGG(S::String, dim_h::Int, σ=relu)
         """"
         dim_h: dimension of vertice embedding
-        dim_e: dimension of edge embedding
         """
 
-        @assert S in ["Mean", "Max", "Sum", "MeanPooling", "MaxPooling", "SumPooling"];
+        @assert S in ["Mean", "Max", "Sum", "Pooling"];
 
-        if S in ["Mean", "Max", "Sum"]
-            return AGG(S, nothing);
+        if S == "Pooling"
+            return AGG(S, Dense(dim_h, dim_h, σ));
         else
-            return AGG(S, Dense(dim_h+dim_e, dim_h+dim_e, σ));
+            return AGG(S, nothing);
         end
     end
 
-    function (c::AGG)(he::Vector)
+    function (c::AGG)(h::Vector{AbstractVector})
         S, L = c.S, c.L;
 
         if S == "Mean"
-            return mean(he);
+            return mean(h);
         elseif S == "Max"
-            return max.(he...);
+            return max.(h...);
         elseif S == "Sum"
-            return sum(he);
-        elseif S == "MeanPooling"
-            return mean(L.(he));
-        elseif S == "MaxPooling"
-            return max.(L.(he)...);
-        elseif S == "SumPooling"
-            return sum(L.(he));
+            return sum(h);
+        elseif S == "Pooling"
+            return max.(L.(h)...);
         end
     end
 
@@ -56,11 +51,11 @@ module GraphSAGE
         z::AbstractVector;
     end
 
-    function SAGE(T::F, k::Int, S::String, dim_h::Int, dim_e::Int, σ=relu) where {F}
-        return SAGE(T, k, AGG(S, dim_h, dim_e, σ), zeros(dim_h+dim_e));
+    function SAGE(T::F, k::Int, S::String, dim_h::Int, σ=relu) where {F}
+        return SAGE(T, k, AGG(S, dim_h, σ), zeros(dim_h));
     end
 
-    function (c::SAGE)(G::AbstractGraph, node_list::Vector{Int}, node_features::Function, edge_features::Function)
+    function (c::SAGE)(G::AbstractGraph, node_list::Vector{Int}, node_features::Function)
         T, k, A, z = c.T, c.k, c.A, c.z;
 
         sampled_nbrs_list = Vector{Vector{Int}}();
@@ -75,19 +70,19 @@ module GraphSAGE
 
         # if this SAGE is not a leaf, then call the child Transformer to get node representation at previous layer
         if T != nothing
-            h0 = T(G, unique_nodes, node_features, edge_features);
+            h0 = T(G, unique_nodes, node_features);
         else
             h0 = [convert(Vector{Float32}, node_features(u)) for u in unique_nodes];
         end
 
         # each vector can be decomposed as [h(v)*, edge_features(v,u)*, h(u)], where * means 'aggregated across v'
-        heh = Vector{AbstractVector}();
+        hh = Vector{AbstractVector}();
         for (u, sampled_nbrs) in zip(node_list, sampled_nbrs_list)
-            he = length(sampled_nbrs) != 0 ? A([vcat(h0[u2i[v]], convert(Vector{Float32}, edge_features(v,u))) for v in sampled_nbrs]) : z;
-            push!(heh, vcat(he, h0[u2i[u]]));
+            hn = length(sampled_nbrs) != 0 ? A([h0[u2i[v]] for v in sampled_nbrs]) : z;
+            push!(hh, vcat(h0[u2i[u]], hn));
         end
 
-        return heh;
+        return hh;
     end
 
     Flux.@treelike SAGE;
@@ -100,16 +95,16 @@ module GraphSAGE
         L::F;
     end
 
-    function Transformer(S::SAGE, dim_h0::Int, dim_h1::Int, dim_e::Int, σ=relu)
-        L = Dense(dim_h0*2+dim_e, dim_h1, σ);
+    function Transformer(S::SAGE, dim_h0::Int, dim_h1::Int, σ=relu)
+        L = Dense(dim_h0*2, dim_h1, σ);
 
         return Transformer(S, L);
     end
 
-    function (c::Transformer)(G::AbstractGraph, node_list::Vector{Int}, node_features::Function, edge_features::Function)
+    function (c::Transformer)(G::AbstractGraph, node_list::Vector{Int}, node_features::Function)
         S, L = c.S, c.L;
 
-        h1 = L.(S(G, node_list, node_features, edge_features));
+        h1 = L.(S(G, node_list, node_features));
 
         return h1;
     end
@@ -119,27 +114,27 @@ module GraphSAGE
 
 
     # graph encoder
-    function graph_encoder(dim_in::Int, dim_out::Int, dim_h::Int, dim_e::Int, layers::Vector{String};
+    function graph_encoder(dim_in::Int, dim_out::Int, dim_h::Int, layers::Vector{String};
                            ks::Vector{Int}=repeat([typemax(Int)], length(layers)), σ=relu)
         @assert length(layers) > 0;
         @assert length(layers) == length(ks);
 
-        sage = SAGE(nothing, ks[1], layers[1], dim_in, dim_e, σ);
+        sage = SAGE(nothing, ks[1], layers[1], dim_in, σ);
         if length(layers) == 1
             # single layer, directly output
-            tsfm = Transformer(sage, dim_in, dim_out, dim_e, σ);
+            tsfm = Transformer(sage, dim_in, dim_out, σ);
         else
             # multiple layer, first encode to hidden
-            tsfm = Transformer(sage, dim_in, dim_h, dim_e, σ);
+            tsfm = Transformer(sage, dim_in, dim_h, σ);
 
             # the inner layers, hidden to hidden
             for i in 2:length(layers)-1
-                sage = SAGE(tsfm, ks[i], layers[i], dim_h, dim_e, σ);
-                tsfm = Transformer(sage, dim_h, dim_h, dim_e, σ);
+                sage = SAGE(tsfm, ks[i], layers[i], dim_h, σ);
+                tsfm = Transformer(sage, dim_h, dim_h, σ);
             end
 
-            sage = SAGE(tsfm, ks[end], layers[end], dim_h, dim_e, σ);
-            tsfm = Transformer(sage, dim_h, dim_out, dim_e, σ);
+            sage = SAGE(tsfm, ks[end], layers[end], dim_h, σ);
+            tsfm = Transformer(sage, dim_h, dim_out, σ);
         end
 
         return tsfm;
