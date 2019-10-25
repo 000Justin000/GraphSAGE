895f6dcd97ba5afab62f9a19b7fb7537b272a17a
Junteng Jia <juntengjia@hotmail.com>
2019-10-25 09:54:24 -0400

    clean up edge features

--- src/GraphSAGE.jl
+++ src/GraphSAGE.jl
@@ -25,7 +25,7 @@ module GraphSAGE
         end
     end
 
-    function (c::AGG)(h::Vector{Vector{F}}) where {F}
+    function (c::AGG)(h::AbstractVector{AbstractVector{F}}) where {F}
         S, L = c.S, c.L;
 
         if S == "Mean"
